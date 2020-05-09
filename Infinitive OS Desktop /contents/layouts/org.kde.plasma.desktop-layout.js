7def036c53543e7d85944db9d58e78d24c8cd569
Infinitive Developer Team <65085719+Infinitive-Developer-team@users.noreply.github.com>
2020-05-09 19:37:54 +0200

    Add files via upload

---
+++ Infinitive OS Desktop /contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,127 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "420",
+                    "DialogWidth": "560"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "top",
+                    "ToolBoxButtonX": "777",
+                    "ToolBoxButtonY": "334",
+                    "filterMimeTypes": "\\0",
+                    "showToolbox": "false",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/BytheWater/contents/images/1920x1080.jpg"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        },
+                        "/Configuration/General": {
+                            "length": "983"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        },
+                        "/Configuration/Appearance": {
+                            "boldText": "true",
+                            "customDateFormat": "ddd ",
+                            "dateFormat": "custom",
+                            "showDate": "true",
+                            "use24hFormat": "2"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "420",
+                            "DialogWidth": "560"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        }
+                    },
+                    "plugin": "org.kde.milou"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "72",
+                    "DialogWidth": "1360"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                }
+            },
+            "height": 1.2857142857142858,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 97.14285714285714,
+            "minimumLength": 97.14285714285714,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
